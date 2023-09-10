06bc6c1ca8253bbca25ddf8a1633d3854842a190
wodls1616 <wodls1616@gmail.com>
2023-09-10 17:58:29 +0900

    회원 테스트 코드를 작성 JUNIT5를 사용하여 테스트 코드를 작성 테스트코드의 Transactional은 Rollback이 기본이므로 테스트 후 데이터가 남아있지 않는다.

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findMemberByName(String name) {
+        return em.createQuery("select m from Member m where m.username =: username", Member.class)
+                .setParameter("username", name)
+                .getResultList();
+    }
+}
