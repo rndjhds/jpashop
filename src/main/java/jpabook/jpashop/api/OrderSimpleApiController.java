1d4385448f74aa918da1f4cd40e201ddb3126204
wodls1616 <wodls1616@gmail.com>
2023-09-18 23:48:37 +0900

    V2는 엔티티를 직접 반환하지 않고 DTO로 감싸서 반환하기 떄문에 양방향 무한로딩이 걸리지 않는 장점이 있다. 하지만 LAZY로딩이라도 N+1의 문제가 남아 있기떄문에 쿼리 성능이 떨어진다. 그래서 V3에서는 페치 조인을 해서 N+1의 문제를 해결할려고 한다. 엔티티는 절대 반환해주면 안된다.

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,70 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.member.Address;
+import jpabook.jpashop.order.Order;
+import jpabook.jpashop.order.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/*
+X to One (ManyToOne, OneToOne) 최적화방법
+* Order
+* Order -> Member
+* Order -> Delivery
+* */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간
+        private OrderStatus orderStatus;
+        private Address address;
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getUsername();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getOrderStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
+}
