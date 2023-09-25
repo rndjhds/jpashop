d9eb99d2d35ed1bcfe657a18bd5e8438b79610c5
wodls1616 <wodls1616@gmail.com>
2023-09-25 23:55:46 +0900

    쿼리가 N+1로 발생하는 문제를 해결하기 위해서 select 절에서 foreach로 반복해서 orderId를 하나씩 조회해주던거에서 IN절로 하나로 묶은 다음 한번에 조회를 시켰다. 거기다가 먼저 조회한 orderId들을 Map에 담아서 보관해주어서 쿼리가 더 실행되는걸 막았다. 떄문에 이전에 2 + 1 문제에서 1 + 1로 쿼리가 줄었다.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -68,6 +68,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Data
     static class OrderDto {
 
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -35,7 +37,30 @@ public class OrderQueryRepository {
         return em.createQuery("select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count) " +
                 "from OrderItem oi " +
                 "join oi.item i " +
-                "where oi.order.id = : orderId").setParameter("orderId", orderId).getResultList();
+                "where oi.order.id = : orderId", OrderItemQueryDto.class).setParameter("orderId", orderId).getResultList();
     }
 
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+        result.forEach(o -> o.setOrderItemQuery(orderItemMap.get(o.getOrderId())));
+
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery("select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count) " +
+                "from OrderItem  oi " +
+                "join oi.item i " +
+                "where oi.order.id in : orderIds", OrderItemQueryDto.class).setParameter("orderIds", orderIds).getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream().collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+        return orderItemMap;
+    }
+
+    private static List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream().map(o -> o.getOrderId()).collect(Collectors.toList());
+    }
 }
